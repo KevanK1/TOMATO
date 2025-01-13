c6a113e98ae462b0ccb125ce8bd748164a1cf1bc
Kevan Tamboli <kevantamboli@gmail.com>
2025-01-13 19:38:02 +0530

    patch changes to dependencies

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const connection = require('./config/dbConnection')
-const model = require();
+const model = require('./model/model');
 
 const app = express();
 
+app.get('/', (req, res) => {
+res.send('Welcome')
+});
+
+app.listen(3000)
--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose
-  .connect("mongodb://localhost:27017/tomato")
+const connection = mongoose.connect("mongodb://localhost:27017/tomato")
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
 
-  module.exports = connection
+module.exports = connection
